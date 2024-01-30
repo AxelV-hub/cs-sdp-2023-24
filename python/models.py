33f8dfd8bf2794d686d5f1e37d121495477d7d1e
AxelV-hub <80691868+AxelV-hub@users.noreply.github.com>
2024-01-30 10:04:21 +0100

    Structure du code

--- python/models.py
+++ python/models.py
@@ -1,6 +1,9 @@
 import pickle
 from abc import abstractmethod
 
+import gurobipy as gp
+from gurobipy import GRB
+
 import numpy as np
 
 
@@ -167,16 +170,20 @@ class TwoClustersMIP(BaseModel):
         ----------
         n_pieces: int
             Number of pieces for the utility function of each feature.
-        n°clusters: int
+        n_clusters: int
             Number of clusters to implement in the MIP.
         """
         self.seed = 123
+        self.n_pieces = n_pieces
+        self.n_clusters = n_clusters
         self.model = self.instantiate()
 
     def instantiate(self):
         """Instantiation of the MIP Variables - To be completed."""
-        # To be completed
-        return
+        # create a model
+        model = gp.Model("MIP")
+
+        return model
 
     def fit(self, X, Y):
         """Estimation of the parameters - To be completed.
@@ -188,9 +195,130 @@ class TwoClustersMIP(BaseModel):
         Y: np.ndarray
             (n_samples, n_features) features of unchosen elements
         """
+        n_samples = X.shape[0]
+        n_features = X.shape[1]
+        n_pieces = self.n_pieces
 
-        # To be completed
-        return
+        # define the variables of the MIP
+        # define a vector of alpha_i_l of size n_features * n_pieces, they will be the first variables of the MIP
+        # alpha_i_l = np.zeros((n_features, n_pieces))
+
+        # define a vector of sigma_j of size 2 * n_samples, they will be the second variables of the MIP
+        # sigma_j = np.zeros((2, n_samples))
+
+        # x = self.model.addVars(alpha_i_l, sigma_j, vtype=GRB.FLOAT, name="x")
+
+        u = {}
+        for k in range(self.n_clusters):
+            for i in range(self.n_features):
+                for l in range(self.n_pieces):
+                    u[k, i, l] = model.addVar(lb=0, ub=1, vtype=GRB.CONTINUOUS, name=f"u_{k}{i}{l}")
+
+        z = {}
+        for k in range(self.n_clusters):
+            for j in range(len(X)):
+                z[j, k] = model.addVar(vtype=GRB.BINARY, name=f"z_{j}_{k}")
+
+        sigma_plus = model.addVar(lb=0, vtype=GRB.CONTINUOUS, name="sigma_plus") 
+        sigma_minus = model.addVar(lb=0, vtype=GRB.CONTINUOUS, name="sigma_minus")
+
+        # Constraints fonction de décision croissantes 
+        for i in range(self.n_features):
+            for k in range(self.n_clusters):
+                for l in range(self.n_pieces - 1):
+                    model.addConstr(u[k, i, l] <= u[k, i, l + 1])
+        
+        #Contrainte d'égalite <-> préférence
+
+        for j in len(X):
+            model.addConstr(np.sum([z[j, k] for k in range(self.n_clusters)])== 1)
+
+        # alpha_i_l = self.model.addMVar(
+        #     shape=(n_features, n_pieces), 
+        #     lb = 0.0,
+        #     vtype=GRB.CONTINUOUS, 
+        #     name="alpha_i_l"
+        # )
+        # sigma_j = self.model.addMVar(
+        #     shape=(2, n_samples), 
+        #     vtype=GRB.CONTINUOUS, 
+        #     name="sigma_j"
+        # )
+
+        # alpha_i_l = self.model.addVars(
+        #     ((i,l) for i in range(n_features) for l in range(n_pieces)),
+        #     vtype=GRB.CONTINUOUS, 
+        #     name="alpha_i_l",
+        #     lb=0.0,
+        #     ub=1.0
+        # )
+        # sigma_j = self.model.addVars(
+        #     ((k,j) for k in [0, 1] for j in range(n_samples)),
+        #     vtype=GRB.CONTINUOUS, 
+        #     name="sigma_j"
+        # )
+
+        # define some preliminary useful coefficients 
+        # define the min_i and max_i for each feature i, computed with X and Y, the value will be later used to calculate the score function
+        min_i = np.zeros(n_features)
+        max_i = np.zeros(n_features)
+        for i in range(n_features):
+            min_i[i] = min(min(X[:, i]), min(Y[:, i]))
+            max_i[i] = max(max(X[:, i]), max(Y[:, i]))
+
+        # define the intervals boundaries x_i_l for each feature i, and l from 0 to n_pieces, computed with min_i and max_i
+        x_i_l = np.zeros((n_features, n_pieces+1))
+        for i in range(n_features):
+            for l in range(n_pieces+1):
+                x_i_l[i, l] = min_i[i] + l * (max_i[i] - min_i[i]) / n_pieces
+
+        # define the score function, based on the alpha_i_l and max_i and min_i
+        def score_function(x_i, alpha_i_l):
+            # define the interval l_i of x_i_l in which x_i is located
+            l_i = np.zeros(n_features)
+            for i in range(n_features):
+                l_i[i] = int(n_pieces * (x_i - min_i[i]) / (max_i[i] - min_i[i]))  # value of l_i between 0 and n_pieces
+
+            # define the score function for each feature i 
+            score = 0
+            for i in range(n_features):
+                score += alpha_i_l[i, l_i[i]] + (x_i - x_i_l[i, l_i]) / (x_i_l[i, l_i+1] - x_i_l[i, l_i]) * (alpha_i_l[i, l_i[i]+1] - alpha_i_l[i, l_i[i]])
+
+            return score
+
+        # define the constraints of the MIP
+        # constraint 1: the sum of the alpha_i_l over i with l fixed at n_pieces is equal to 1
+        self.model.addConstr(gp.quicksum(alpha_i_l[t, n_pieces] for t in range(n_samples)) == 1)
+
+        # constraint 2: alpha_i_l is between 0 and 1 for all i and l
+        # self.model.addConstrs(alpha_i_l[i, l] >= 0 for i in range(n_features) for l in range(n_pieces))
+
+        # constraint 3: alpha_i_l[i, l] <= alpha_i_l[i, l+1] for all i and l
+        self.model.addConstrs(alpha_i_l[i, l] <= alpha_i_l[i, l+1] for i in range(n_features) for l in range(n_pieces))
+
+        # constraint 4: for each pair of X[j],Y[j], score_function(X[j]) + sigma_j[0, j] >= score_function(Y[j]) + sigma_j[1, j]
+        self.model.addConstrs(score_function(X[j,:], alpha_i_l) + sigma_j[0, j] >= score_function(Y[j,:], alpha_i_l) + sigma_j[1, j] for j in range(n_samples))
+
+        # define the objective function of the MIP
+        # objective function: minimize the sum of abs(sigma_j)
+        self.model.setObjective(gp.quicksum(abs(sigma_j[k,j]) for k in [0,1] for j in range(n_samples)), GRB.MINIMIZE)
+
+        # optimize the model
+        self.model.optimize()
+
+        # get the optimal values of alpha_i_l 
+        if self.model.status == GRB.OPTIMAL:
+            print('Optimal found')
+            alpha_i_l_opt = np.zeros((n_features, n_pieces))
+            sigma_j_opt = np.zeros((2, n_samples))
+            for i in range(n_features):
+                for l in range(n_pieces):
+                    alpha_i_l_opt[i, l] = x[i, l].x
+        else:
+            print('No solution')
+
+        return alpha_i_l_opt
+
 
     def predict_utility(self, X):
         """Return Decision Function of the MIP for X. - To be completed.
@@ -203,6 +331,28 @@ class TwoClustersMIP(BaseModel):
         # To be completed
         # Do not forget that this method is called in predict_preference (line 42) and therefor should return well-organized data for it to work.
         return
+
+
+
+import os
+import sys
+
+# sys.path.append("../python")
+
+import matplotlib.pyplot as plt
+import numpy as np
+from data import Dataloader
+import metrics
+
+
+data_loader = Dataloader("data\dataset_4") # Specify path to the dataset you want to load
+X, Y = data_loader.load()
+
+# model = RandomExampleModel() # Instantiation of the model with hyperparameters, if needed
+model = TwoClustersMIP(n_pieces=5, n_clusters=1)
+model.fit(X, Y) 
+
+
 
 
 class HeuristicModel(BaseModel):
